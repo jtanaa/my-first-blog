c4e908c9b02ae528bc2d0ea23d40ba3f0d22324e
jtanaa <jtanhkust@gmail.com>
2016-04-26 12:35:01 +0800

    Store as draft, publish later. and delete draft, delete post

--- blog/models.py
+++ blog/models.py
@@ -31,6 +31,7 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
 
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
